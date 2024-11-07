9b9efb3d93743b87485fbb6e7f32298b3e425bf5
duonanotte <166713796+duonanotte@users.noreply.github.com>
2024-11-07 14:24:28 +0300

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -10,18 +10,19 @@ class Settings(BaseSettings):
     USE_RANDOM_DELAY_IN_RUN: bool = False
     RANDOM_DELAY_IN_RUN: list[int] = [5, 39600]
 
-    AUTO_TAP: bool = False
+    REF_LINK: str = "https://t.me/cexio_tap_bot?start=1716965343267932"
+
+    AUTO_TAP: bool = True
     RANDOM_TAPS_COUNT: list = [25, 75]
     SLEEP_BETWEEN_TAPS: list = [25, 35]
+    SLEEP_BY_MIN_ENERGY: int = 250
     AUTO_CONVERT: bool = True
     MINIMUM_TO_CONVERT: float = 0.1
     AUTO_BUY_UPGRADE: bool = True
-    AUTO_TASK: bool = False
+    AUTO_TASK: bool = True
     AUTO_CLAIM_SQUAD_BONUS: bool = False
 
-    USE_PROXY: bool = False
+    USE_PROXY: bool = True
 
 
 settings = Settings()
-
-
